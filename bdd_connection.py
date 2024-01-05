61d02d8c70656028d56a8038e8720c5e0cc87e74
Cristian Xavier Tapia Vargas <thecristianx@hotmail.com>
2024-01-05 13:05:45 -0500

    ft - conexion base de datos sql

--- bdd_connection.py
+++ bdd_connection.py
@@ -9,6 +9,16 @@ PASSWORD = "$M$RUsuario"
 # Url de conexión con sqlalchemy
 conn_url = f"mssql+pyodbc://{USERNAME}:{PASSWORD}@{SERVER}/{DATABASE}?driver=ODBC+Driver+17+for+SQL+Server"
 
+
+# Url de conexión con sqlalchemy para PostgreSQL
+#conn_url = f"postgresql+psycopg2://{USERNAME}:{PASSWORD}@{SERVER}:{PORT}/{DATABASE}"
+
+# Url de conexión con sqlalchemy para Oracle
+#conn_url = f"oracle+cx_oracle://{USERNAME}:{PASSWORD}@{SERVER}:{PORT}/?service_name={DATABASE}"
+
+# Url de conexión con sqlalchemy para MariaDB
+#conn_url = f"mysql+mysqldb://{USERNAME}:{PASSWORD}@{SERVER}:{PORT}/{DATABASE}"
+
 engine = create_engine(conn_url)
 
 # Conexión
